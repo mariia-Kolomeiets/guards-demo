4f91e95d12767e41d9ae8ec9826cd6e7ccbe507c
Mariia Kolomeiets <Mariia_Kolomeiets@epam.com>
2022-04-05 19:25:55 +0300

    add files

--- src/app/article/article.component.ts
+++ src/app/article/article.component.ts
@@ -2,10 +2,7 @@ import {Component} from '@angular/core';
 
 @Component({
   template: `<h2>Welcome to Article Home</h2>
-  <p>Find article <a routerLink="list" routerLinkActive="active">list</a></p>
-  <div [ngClass]="'child-container'">
-    <router-outlet></router-outlet>
-  </div>
+  <router-outlet></router-outlet>
   `
 })
 export class ArticleComponent {
